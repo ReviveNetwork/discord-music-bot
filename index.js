33e316ad252d396c59644fcfccc5da22bde82801
meldsza <meldsza@gmail.com>
2017-04-24 10:01:46 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var serverID = "184536578654339072";
+var textChannelID = "275880097460781057";
+var voiceChannelID = "275157297318068225";
+var aliasesFile = "alias.json";
+var botToken = "";
+bot.setYoutubeKey("");
+
+bot.run(serverID, textChannelID, voiceChannelID, aliasesFile, botToken);
